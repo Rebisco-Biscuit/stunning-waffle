f22c1e970b5a2767f14f5cbfc8d092e398105fd6
Expired_Biskwit <143090289+Rebisco-Biscuit@users.noreply.github.com>
2025-03-12 10:55:24 +0800

    Update DMA.c

--- WEEK_1/DMA.c
+++ WEEK_1/DMA.c
@@ -2,6 +2,7 @@
 Dynamic Memory Allocation
 
 Write a function that dynamically allocates an integer array of size n, fills it with values from 1 to n, prints them, and then frees the memory.
+Modify Problem 1 to allow the user to increase the array size using realloc(), appending new values to the end.
 
 int* createArray(int n);
 */
@@ -14,10 +15,13 @@ int* createArray(int n);
 int main() {
     int *a = createArray(5);
     
-    for(int i=0; i<5; i++){ 
-        printf("%d ", a[i]); 
-    }
-    free(a);
+    for(int i=0; i<5; i++){ printf("%d ", a[i]); }
+
+    // Problem 2:
+    int *new_a = realloc(a,sizeof(int)*8);
+    for(int i=0; i<8; i++){ printf("%d ", new_a[i]=i); }
+    
+    free(new_a);
 }
 
 int* createArray(int n){
