61f08586b895763972e6187a5f0666471ce4e4f0
Expired_Biskwit <143090289+Rebisco-Biscuit@users.noreply.github.com>
2025-03-13 23:21:56 +0800

    Update 2D-Allocation.c

--- WEEK_1/2D-Allocation.c
+++ WEEK_1/2D-Allocation.c
@@ -8,6 +8,7 @@ Write a function int** create2DArray(int rows, int cols) that dynamically alloca
 
 int** create2DArray(int rows, int cols);
 void freeMem(int **arr, int rows);
+void transposeMatrix(int** matrix, int rows, int cols);
 
 int main() {
     int rows = 2, cols = 3;
@@ -15,6 +16,7 @@ int main() {
     
     if(myArr == NULL) { printf("Couldn't create matrix."); }
     
+    printf("Matrix:\n");
     for(int i=0; i<rows; i++) {
         for(int j=0; j<cols; j++) {
             printf("%d ", myArr[i][j]);
@@ -22,9 +24,36 @@ int main() {
         printf("\n");
     }
     
+    transposeMatrix(myArr, rows, cols);
+    
     freeMem(myArr, rows);
     
     return 0;
+}
+
+void transposeMatrix(int** matrix, int rows, int cols) {
+    //columns becomes the rows, vice versa.
+    int** newMatrix = malloc(cols * sizeof(int*));
+    
+    if(newMatrix == NULL) { exit; }
+    
+    for(int i=0; i<cols; i++) {
+        newMatrix[i] = malloc(rows * sizeof(int));
+    }
+    
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<cols; j++) {
+            newMatrix[j][i] = matrix[i][j];
+        }
+    }
+    
+    printf("Transposed Matrix (NxM):\n");
+    for(int i=0; i<cols; i++) {
+        for(int j=0; j<rows; j++) {
+            printf("%d ", newMatrix[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 void freeMem(int **arr, int rows) {
